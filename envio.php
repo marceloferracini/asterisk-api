d790aa73145d18f2c622082dc12a5803d49480ad
Leandro Barbosa <leandro@canoadigital.com>
2018-08-17 15:43:32 -0300

    change structure and add call dialogflow function

--- envio.php
+++ envio.php
@@ -9,19 +9,19 @@
     main($argv);
 */
 
+/*
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
-
+*/
 
 include "Jobs/Asterisk.php";
-echo "a";
 
 function main($array_files){
 
     $asterisk = new Asterisk($array_files);
     return $asterisk->control();
 }
-main(array('../teste.wav', '../teste.wav', '../teste.wav'));
+//main(array('../teste.wav', '../teste.wav', '../teste.wav'));
 
 ?>
