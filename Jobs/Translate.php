e65cde809cfa15f7c1e2cbbf7b1eaad9234c31cf
marcelobferracini <marcelo.ferracini@canoadigital.com>
2021-07-03 12:20:20 -0300

    update-03-JUL-12h08

--- Jobs/Translate.php
+++ Jobs/Translate.php
@@ -1,30 +1,15 @@
 <?php
 
-use App\Services\S3Service;
-
-// Imports the Google Cloud client library
-use Google\Cloud\Speech\SpeechClient;
-
-// Used on google Text to Speech
-use GuzzleHttp;
-
-require __DIR__.'/../vendor/autoload.php';
-
 
 class Translate {
 
-    public function  __construct()
-    {
-        //load .env file
-        $this->dotenv = new Dotenv\Dotenv(__DIR__ . "/../");
-        $this->dotenv->load();
+    public function TranslateTextToSpeech($message) {
 
-    }
+        // creating AGI object
+        $agi = new AGI();
 
-    function TranslateTextToSpeech($message) {
-
-        $file_url = "";
-        $this->agi->exec("NOOP", "CAIU AQUI DENTRO PELO MENOS ");
+        //$file_url = "";
+        $agi->exec("NOOP", "CAIU AQUI DENTRO PELO MENOS ");
 
         //check if this text exist on DB
         //$records = $this->iTranslationsRepository->getFileByMessage( $request->message )->toArray();
@@ -37,7 +22,7 @@ class Translate {
 
             //call api to translate   gateway can be: ['AWS' or 'Google']
             //$translationGateway = new TranslationGateways('Google');
-            $audio = $this->googleTextToSpeech($message);
+            //$audio = $this->googleTextToSpeech($message);
 
             //put the file on s3
             // $s3Service = new S3Service();
@@ -52,46 +37,46 @@ class Translate {
 
         //}
 
-        return $audio;
+        //return $audio;
     }
 
-    function googleTextToSpeech($text)
-    {
+    // function googleTextToSpeech($text)
+    // {
 
-        $googleAPIKey = env('GOOGLE_API_KEY');
+    //     $googleAPIKey = env('GOOGLE_API_KEY');
 
-        $client = new GuzzleHttp\Client();
-        $requestData = [
-            'input' =>[
-                'text' => $text
-            ],
-            'voice' => [
-                'languageCode' => 'pt-BR',
-                'name' => 'pt-BR-Standard-A'
-            ],
-            'audioConfig' => [
-                'audioEncoding' => 'MP3',
-                'pitch' => 0.00,
-                'speakingRate' => 1.00
-            ]
-        ];
+    //     $client = new GuzzleHttp\Client();
+    //     $requestData = [
+    //         'input' =>[
+    //             'text' => $text
+    //         ],
+    //         'voice' => [
+    //             'languageCode' => 'pt-BR',
+    //             'name' => 'pt-BR-Standard-A'
+    //         ],
+    //         'audioConfig' => [
+    //             'audioEncoding' => 'MP3',
+    //             'pitch' => 0.00,
+    //             'speakingRate' => 1.00
+    //         ]
+    //     ];
 
-        try {
-            $response = $client->request('POST', 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=' . $googleAPIKey, [
-                'json' => $requestData
-            ]);
-        } catch (Exception $e) {
-            die('Something went wrong: ' . $e->getMessage());
-        }
+    //     try {
+    //         $response = $client->request('POST', 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=' . $googleAPIKey, [
+    //             'json' => $requestData
+    //         ]);
+    //     } catch (Exception $e) {
+    //         die('Something went wrong: ' . $e->getMessage());
+    //     }
 
-        $fileData = json_decode($response->getBody()->getContents(), true);
+    //     $fileData = json_decode($response->getBody()->getContents(), true);
 
-        $result['AudioStream'] = base64_decode($fileData['audioContent']);
-        $result['file_name'] = uniqid().'-Google.mp3';
+    //     $result['AudioStream'] = base64_decode($fileData['audioContent']);
+    //     $result['file_name'] = uniqid().'-Google.mp3';
 
-        return $result;
+    //     return $result;
 
-    }
+    // }
     
 
 
